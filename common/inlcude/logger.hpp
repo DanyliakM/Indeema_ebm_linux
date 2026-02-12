2e494591e3a49639b4b830aed8254bf959a27d11
Myroslav <danyliakmyroslav@gmail.com>
2026-02-12 16:34:08 +0000

    Changes with guards

--- common/inlcude/logger.hpp
+++ common/inlcude/logger.hpp
@@ -1,6 +1,11 @@
+#ifndef LOGGER_HPP
+#define LOGGER_HPP
+
 #include <vector>
 #include <string>
 #include <iostream>
+
+
 
 class Logger {
     std::vector<std::string> history; 
@@ -8,3 +13,5 @@ public:
     void log(const std::string& message);
     void printHistory();
 };
+
+#endif
