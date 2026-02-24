74a90160d7e990ec718d4fa694b6dd3ce946b439
Myroslav <danyliakmyroslav@gmail.com>
2026-02-24 15:44:37 +0000

    fixed file with parts

--- common/include/JsonParser.hpp
+++ common/include/JsonParser.hpp
@@ -1,5 +1,8 @@
 #pragma once
 #include <string>
+#include <serial/serial.h> 
+#include <unistd.h>        
+#include "logger.hpp"      
 
 struct UartDevInfo {
     std::string path;
@@ -9,4 +12,39 @@ struct UartDevInfo {
 class JsonParser {
 public:
     static bool parseConfig(const std::string& filePath, UartDevInfo& info);
+};
+
+class UARTService {
+private:
+    serial::Serial _serial;
+public:
+    UARTService(const std::string& port, int baud) {
+        _serial.setPort(port);
+        _serial.setBaudrate(baud);
+        serial::Timeout to = serial::Timeout::simpleTimeout(1000);
+        _serial.setTimeout(to);
+    }
+
+    bool start() {
+        try {
+            _serial.open();
+            _serial.setFlowcontrol(serial::flowcontrol_none);
+            return _serial.isOpen();
+        } catch (...) { return false; }
+    }
+
+    void testEcho(const std::string& msg) {
+        Logger::info("Відправка в порт: " + msg);
+        _serial.write(msg);
+        _serial.flush(); 
+
+        usleep(100000); 
+
+        if (_serial.available() > 0) {
+            std::string result = _serial.read(_serial.available());
+            Logger::info("УСПІХ! Отримано ехо: " + result);
+        } else {
+            Logger::error("Ехо не повернулося.  RX-TX!");
+        }
+    }
 };
--- main.cpp
+++ main.cpp
@@ -1,49 +1,12 @@
 #include <iostream>
-#include <serial/serial.h>
-#include <unistd.h>
 #include "logger.hpp"
 #include "JsonParser.hpp"
-
-class UARTService {
-private:
-    serial::Serial _serial;
-public:
-    UARTService(const std::string& port, int baud) {
-        _serial.setPort(port);
-        _serial.setBaudrate(baud);
-        serial::Timeout to = serial::Timeout::simpleTimeout(1000);
-        _serial.setTimeout(to);
-    }
-
-    bool start() {
-        try {
-            _serial.open();
-            _serial.setFlowcontrol(serial::flowcontrol_none);
-            return _serial.isOpen();
-        } catch (...) { return false; }
-    }
-
-    void testEcho(const std::string& msg) {
-        Logger::info("Відправка в порт: " + msg);
-        _serial.write(msg);
-        _serial.flush(); 
-
-        usleep(100000); 
-
-        if (_serial.available() > 0) {
-            std::string result = _serial.read(_serial.available());
-            Logger::info("УСПІХ! Отримано ехо: " + result);
-        } else {
-            Logger::error("Ехо не повернулося. Перевір перемичку RX-TX!");
-        }
-    }
-};
 
 int main() {
     UartDevInfo devInfo;
 
     if (!JsonParser::parseConfig("config.json", devInfo)) {
-        Logger::error("Зупинка програми через помилку конфігурації.");
+        Logger::error("Зупинка програми, помилкa конфігурації.");
         return 1;
     }
 
