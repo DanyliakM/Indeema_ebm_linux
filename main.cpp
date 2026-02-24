e3b1349fe6a807ba26be3d09d3d7421568dc21fc
Myroslav <danyliakmyroslav@gmail.com>
2026-02-24 15:49:35 +0000

    finish uart communication

--- main.cpp
+++ main.cpp
@@ -5,14 +5,15 @@
 int main() {
     UartDevInfo devInfo;
 
-    if (!JsonParser::parseConfig("config.json", devInfo)) {
-        Logger::error("Зупинка програми, помилкa конфігурації.");
-        return 1;
-    }
+
 
     UARTService uart(devInfo.path, devInfo.baudrate);
     
     if (uart.start()) {
+         if (!JsonParser::parseConfig("config.json", devInfo)) {
+            Logger::error("Зупинка програми, помилкa конфігурації.");
+            return 1;
+        }
         Logger::info("Порт " + devInfo.path + " успішно відкрито.");
         uart.testEcho("Indeema_Echo_Test");
     } else {
